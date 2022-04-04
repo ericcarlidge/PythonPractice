aeb699f0e55c531389976966b0a6395f9721f229
ericcarlidge <100529980+ericcarlidge@users.noreply.github.com>
2022-04-04 11:42:19 -0400

    Create practice3_newlist.py

---
+++ practice3_newlist.py
@@ -0,0 +1,12 @@
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+
+entry = input("Please enter a number in the rane 1 to 90: ")
+num = int(entry)
+
+for i in a:
+    if i < num: b.append(i)
+
+print(b)
