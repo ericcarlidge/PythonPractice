96149ceffea0d79f84a4202d418f0ea45387c58e
ericcarlidge <100529980+ericcarlidge@users.noreply.github.com>
2022-04-06 15:39:44 -0400

    Update practice9_guessgame1.py

--- practice9_guessgame1.py
+++ practice9_guessgame1.py
@@ -1,20 +1,25 @@
 # Generate a random number between 1 and 9 (including). Ask the user to guess the number, 
-# then tell them if they're too high. too low, or correct until they guess correctly
+# then tell them if they're too high. too low, or correct until they guess correctly.
 
 from random import randint
 
 num = randint(1, 9)
 
 def guessinggame():
+    count = 0
     while True:
-        guess = int(input("Guess a random number between 1 and 9: "))
+        guess = int(input("Guess a number between 1 and 9: "))
     
         if guess > num:
+            count += 1
             print("Your guess is too high.")
         elif guess < num:
+            count += 1
             print("Your guess is too low.")
         else:
+            count += 1
             print("You are correct!")
+            print("You guessed the number in " + str(count) + " guess(es)!")
             return
 
 guessinggame()
